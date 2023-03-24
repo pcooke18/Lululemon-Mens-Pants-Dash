06a5bbd6c42fffa0a8178e32f78996d184a2508e
pcooke18 <pierson.cooke@vanderbilt.edu>
2023-03-24 01:23:51 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,434 @@
+from dashFunctions import add_data, generate_bigrams, generate_trigrams, generate_plot
+
+#Dash
+from dash import Dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+
+#Graphing
+import plotly.express as px
+
+#Generic
+from collections import OrderedDict
+import pandas as pd
+import base64
+from io import BytesIO
+import base64
+
+
+data = add_data()
+
+multi_select = {
+    'Product Family': ['Other', 'ABC', 'Pull-On', 'Commission', 'Unspecified Jogger', 'License to Train', 'Surge', 'City Sweat', 'Running', 'Lab', 'Hiking', 'New Venture', 'Balancer', 'Discipline'],
+    'Product Design': ['On the Move', 'Golf', 'Training', 'Running', 'Yoga', 'Hiking', 'Running and Training']
+}
+
+external_style = ['/bootstrap.css']
+
+app = Dash(__name__, external_stylesheets=external_style)
+
+app.layout = html.Div(
+    [
+        html.Div(
+            children = [
+                html.H2(
+                    children = 'Lululemon Mens Pants Dashboard',
+                    style = {
+                        'textAlign': 'center',
+                        'fontSize': 32
+                        }
+                ), 
+
+                html.P(
+                    children = 'Use this dashboard to explore reviews of Lululemon mens pants posted to their official website. Data was collected on March 16, 2023.',
+                    style = {
+                        'textAlign': 'center',
+                        'fontSize': 22
+                        }
+                )
+            ]
+
+        ),
+
+        html.Br(),
+        html.Hr(),
+        
+        html.Div(
+            children = [
+                html.H2(
+                    children='Section 1: General Overview',
+                    style={
+                        'textAlign': 'left',
+                        'fontSize': 26
+                        }
+                    ),
+
+                    html.P(
+                        children='The bar charts  show the total number of products and reviews throughout the dataset.\
+                        The data can be grouped by either family, design, or fabric in order to get a better sense of trends in the data. Please note the use of a logged scale for the first bar chart.\
+                        The bar chart on the left shows total number of reviews while the right shows total number of products.', 
+                        style={
+                            'textAlign': 'left',
+                            'fontSize': 18
+                        }
+                    ),
+
+                    html.Br(),
+                    html.Br(),
+
+                    html.Div(
+                        [
+                            html.Label('Please select one of the following categories:'),
+                            html.Br(),
+                            dcc.Dropdown(
+                                options=[
+                                    {'label': 'Product Family', 'value': 'Product Family'},
+                                    {'label': 'Product Design', 'value': 'Product Design'},
+                                    {'label': 'Product Fabric', 'value': 'Product Fabric'}
+                                ],
+                                id='column-drop-down',
+                                value='Product Family'
+                            ),
+                            html.Div(
+                                [
+                                    dcc.Graph(id='review-bar-graph', style={'display': 'inline-block'}),
+                                    dcc.Graph(id='product-bar-graph', style={'display': 'inline-block'})
+                                ]
+                            ), 
+                            html.Br()
+                        ]
+                    )
+            ],
+            style={'text-align': 'center'}
+        ),
+        
+        html.Br(),
+        html.Hr(),
+
+        html.Div(
+            children = [
+                html.Div(
+                    children=[
+                        html.H2(
+                            children='Section 2: Product Rating Analysis',
+                            style={
+                                'textAlign': 'left',
+                                'fontSize': 26
+                            }
+                        ),
+
+                        html.P(
+                            children='The graph on the left is a stacked bar which highlights the relative percentage of reviews within a category. \
+                                The 5 star reviews appear at the top of the bar, the 1 star reviews appear at the bottom, and the actual count of the number of reviews is displayed on the bar itself. \
+                                The line graph on the right shows the average rating for the given category at different time periods. Feel free to toggle between inputs and change the date range.',
+                            style={
+                                'textAlign': 'left',
+                                'fontSize': 18
+                            }
+                        )
+                    ]
+                ),
+
+                html.Br(),
+
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.Label('Please select one of the following inputs:'),
+                                html.Br(),
+                                dcc.Dropdown(
+                                    options=[
+                                        {'label': 'Product Family', 'value': 'Product Family'},
+                                        {'label': 'Product Design', 'value': 'Product Design'}
+                                    ],
+                                    id='category-select',
+                                    value='Product Family',
+                                    style={'width':'250px', 'display': 'inline-block'}
+                                )
+                            ],
+                            width={'size': 4, 'offset': 1}
+                        ),
+
+                        html.Br(), 
+                        
+                        dbc.Col(
+                            [
+                                html.Label('Choose the date range below:'),
+                                html.Br(),
+                                dcc.Slider(
+                                    id='year-slider',
+                                    min=int(data['Year of Review'].min()),
+                                    max=int(data['Year of Review'].max()),
+                                    value=int(data['Year of Review'].max()),
+                                    marks={str(year): str(year) for year in data['Year of Review'].unique()}
+                                )
+                            ],
+                            width={'size': 5, 'offset': 1}
+                        )
+                    ],
+                    justify="center",
+                    align="center"
+                ),
+
+                html.Br(),
+
+                html.Div(
+                    children=[
+                        dcc.Graph(id='stacked-bar', style={'display': 'inline-block'}),
+                        dcc.Graph(id='moving-average', style={'display': 'inline-block'})
+                    ]
+                )
+            ],
+            style={'text-align': 'center'}
+        ), 
+
+        html.Br(),
+        html.Hr(),
+
+        html.Div(
+            children = [
+                html.Div(
+                    children=[
+                        html.H2(
+                            children='Section 3: Review Text Analysis',
+                            style={
+                                'textAlign': 'left',
+                                'fontSize': 26
+                            }
+                        ),
+
+                        html.P(
+                            children='The word clouds below show the most common words and phrases that appear within the text of reviews. \
+                            The cloud on the left shows the most common words in reviews with ratings less than 3 starts while the cloud on the right shows the most common words in reviews with more than 3 stars. \
+                            Toggle between options to better understand strengths and areas of improvement for each product group.',
+                            style={
+                                'textAlign': 'left',
+                                'fontSize': 18
+                            }
+                        )
+                    ]
+                ),
+
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.Label('Please select one of the following inputs:'),
+                                html.Br(),
+                                dcc.RadioItems(
+                                    list(multi_select.keys()),
+                                    value = 'Product Family', 
+                                    id='radio-cat-input',
+                                    inline=True
+                                ),
+                            ],
+                            width={'size': 4, 'offset': 0}
+                        ),
+
+                        html.Br(),
+
+                        dbc.Col(
+                            [
+                                html.Label('Please select one of the following options:'),
+                                html.Br(),
+                                dcc.RadioItems(id='radio-cat-output', inline=True)
+                            ],
+                            width={'size': 4, 'offset': 0}
+                        )
+                    ],
+                    justify="center",
+                    align="center"
+                ),
+
+                html.Br(),
+
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.Label('Please select one of the following n-gram types:'),
+                                html.Br(),
+                                dcc.RadioItems(
+                                    id='ngram-input',
+                                    options = [
+                                        dict(label = 'token', value = 'token'),
+                                        dict(label = 'bigram', value = 'bigram'),
+                                        dict(label = 'trigram', value = 'trigram')],
+                                    value = 'token',
+                                    inline=True
+                                )
+                            ],
+                            width={'size': 4, 'offset': 1}
+                        ),
+
+                        html.Br(),
+
+                        dbc.Col(
+                            [
+                                html.Label('Please select number of phrases to display:'),
+                                html.Br(),
+                                dcc.Slider(10, 25, 1, id = 'ngram-count', value=10)
+                            ],
+                            width={'size': 4, 'offset': 1}
+                        ), 
+                    ]
+                ),
+
+                html.Br(),
+                html.Br(),
+
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.Label('N-grams of Negative Reviews (Rating < 3)'), 
+                                html.Br(),
+                                html.Img(id = 'wc-image1', style={'display': 'inline-block'})
+                            ], 
+                            width={'size': 4, 'offset': 1}
+                        ), 
+
+                        html.Br(),
+
+                        dbc.Col(
+                            [
+                                html.Label('N-grams of Positive Reviews (Rating > 3)'), 
+                                html.Br(),
+                                html.Img(id = 'wc-image2', style={'display': 'inline-block'})
+
+                            ]
+                        )
+                    ],
+                    style={'text-align': 'center'}
+                )
+            ],
+            style={'text-align': 'center'}
+        ), 
+
+        html.Br()
+    ], 
+    style={'padding': '5px'}
+)
+
+@app.callback(
+    Output('review-bar-graph', 'figure'),
+    Input('column-drop-down', 'value')
+)
+
+def update_review_bar(value):
+    data1 = data.groupby(value).size().sort_values(ascending=False).reset_index(name='Number of Reviews')
+    fig = px.bar(data1, x=value, y='Number of Reviews', log_y=True)
+    fig.update_layout(title_text= 'Number of Reviews by ' + str(value), title_x=0.5)
+    return fig
+
+@app.callback(
+    Output('product-bar-graph', 'figure'),
+    Input('column-drop-down', 'value')
+)
+
+def update_product_bar(value):
+    data2 = data.groupby('Product ID').nth(0).groupby(value)[value].count().sort_values(ascending=False).reset_index(name='Number of Products')
+    fig = px.bar(data2, x = value, y = 'Number of Products')
+    fig.update_layout(title_text= 'Number of Products by ' + str(value), title_x=0.5)
+    return fig
+
+
+@app.callback(
+    Output('moving-average', 'figure'),
+    [Input('year-slider', 'value'),
+    Input('category-select', 'value')]
+)
+
+def update_line(year, value):
+    df = data[data['Year of Review'] <= year]
+
+    # Split the data before and after March 16 (a year to the day of the latest review in dataframe)
+    df_before = df[df['Date of Review'] < '2022-03-16']
+    df_before['YearMonth'] = df_before['Date of Review'].apply(lambda x: x.strftime('%Y-%m'))
+    df_before = df_before.groupby([value, 'YearMonth'])['Rating (out of 5)'].mean().reset_index()
+    df_before = df_before.rename(columns={'Rating (out of 5)': 'Avg Rating'})
+
+    df_after = df[df['Date of Review'] >= '2022-03-16']
+    df_after['YearMonth'] = df_after['Date of Review'].apply(lambda x: x.strftime('%Y-%m'))
+    df_after = df_after.groupby([value, 'YearMonth'])['Rating (out of 5)'].mean().reset_index()
+    df_after = df_after.rename(columns={'Rating (out of 5)': 'Avg Rating'})
+
+    df = pd.concat([df_before, df_after], ignore_index=True)
+
+    fig = px.line(df, x='YearMonth', y='Avg Rating', color=value)
+    fig.update_layout(yaxis_title="Average Rating (out of 5)", xaxis_title = "Date", 
+    title_text= 'Average Product Rating Over Time', title_x=0.5)
+    return fig
+
+@app.callback(
+    Output('stacked-bar', 'figure'),
+    [Input('year-slider', 'value'),
+    Input('category-select', 'value')]
+)
+
+def update_stacked(year, value):
+    data2 = data[data['Year of Review'] <= year]
+    data2['Rating Category'] = pd.cut(data2['Rating (out of 5)'], bins=[0, 1, 2, 3, 4, 5], labels=['1', '2', '3', '4', '5'])
+
+    grouped = data2.groupby([value, 'Rating Category']).size().reset_index(name='Count')
+    grouped['Percentage'] = grouped.groupby(value)['Count'].apply(lambda x: x / x.sum() * 100)
+
+    colors = ['#d7191c','#fdae61','#ffffbf','#a6d96a','#1a9641']
+
+    fig = px.bar(grouped, x=value, y='Percentage', color='Rating Category', barmode='stack', text='Count', 
+    color_discrete_map={'1': colors[0], '2': colors[1], '3': colors[2], '4': colors[3], '5': colors[4]})
+
+    fig.update_layout(title='Percentage of Ratings by ' + str(value) +  ' and Rating Category', title_x=0.5, 
+                    xaxis_title=value,
+                    yaxis_title='Percentage of Ratings')
+    return fig
+
+
+@app.callback(
+    Output('radio-cat-output', 'options'),
+    Input('radio-cat-input', 'value'))
+def display_categories(categorical_var):
+    return [{'label': i, 'value': i} for i in multi_select[categorical_var]]
+
+
+@app.callback(
+    Output('radio-cat-output', 'value'),
+    Input('radio-cat-output', 'options'))
+def display_options(available_options):
+    return available_options[0]['value']
+
+@app.callback(
+    Output('wc-image1', 'src'),
+    [Input('radio-cat-input', 'value'),
+    Input('radio-cat-output', 'value'),
+    Input('ngram-input', 'value'),
+    Input('ngram-count', 'value')]
+)
+def update_word_cloud(column_select, value_select, ngram_type, top_n):
+    df = data[data['Rating (out of 5)'] < 3]
+    df = df[df[column_select] == value_select]
+
+    #Added to code
+    img = BytesIO()
+    generate_plot(df['Review Text Token'], ngram_type, top_n).save(img, format='PNG') #fig = ... (not including .save()); return fig if this doesn't work
+    return 'data:image/png;base64,{}'.format(base64.b64encode(img.getvalue()).decode())
+
+
+@app.callback(
+    Output('wc-image2', 'src'),
+    [Input('radio-cat-input', 'value'),
+    Input('radio-cat-output', 'value'),
+    Input('ngram-input', 'value'),
+    Input('ngram-count', 'value')]
+)
+def update_word_cloud(column_select, value_select, ngram_type, top_n):
+    df = data[data['Rating (out of 5)'] > 3]
+    df = df[df[column_select] == value_select]
+    fig = generate_plot(df['Review Text Token'], ngram_type, top_n)
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ dashFunctions.py
@@ -0,0 +1,72 @@
+from dash import Dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import collections
+from PIL import Image
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import dash
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
+import re
+import base64
+import io
+
+
+def add_data():
+    reviews = pd.read_csv('/Users/piersoncooke/GitHub Practice Repos/lululemon-scrape/Data/reviews_tokenized3.csv', index_col=False, parse_dates=['Date of Review'])
+    reviews['Review Text Token'] = [re.sub(r'[^A-Za-z\s]', '', x).split() for x in reviews['Review Text Token']]
+
+    products = pd.read_csv('/Users/piersoncooke/GitHub Practice Repos/lululemon-scrape/Data/all_products_updated3.csv', index_col = False)
+    products = products[['Product_ID', 'Product Family', 'Product Design', 'Product Fabric']]
+
+    data = pd.merge(reviews, products, left_on = 'Product ID', right_on = 'Product_ID', suffixes=('', '_y'))
+    data = data.drop('Product_ID', axis = 1)
+    data = data.drop('Customer Name', axis = 1)
+
+    data['Date of Review'] = pd.to_datetime(data['Date of Review'])
+    data['Year of Review'] = data['Date of Review'].fillna(datetime.now()).apply(lambda x: x.strftime('%Y'))
+    data['Year of Review'] = pd.to_numeric(data['Year of Review']) 
+
+    return data
+
+
+def generate_bigrams(tokens):
+    return list(zip(tokens[:-1], tokens[1:]))
+
+def generate_trigrams(tokens):
+    return list(zip(tokens[:-2], tokens[1:-1], tokens[2:]))
+
+def generate_plot(token_list, ngram_type = 'token', top_ngrams = 10):
+    if ngram_type == 'token':
+        flat_token_list = [token for sublist in token_list for token in sublist]
+        token_counter = collections.Counter(flat_token_list)
+        most_common = token_counter.most_common(top_ngrams)
+
+    elif ngram_type == 'bigram':
+        bigram_list = [generate_bigrams(tokens) for tokens in token_list]
+        flat_bigram_list = [bigram for sublist in bigram_list for bigram in sublist]
+        bigram_counter = collections.Counter(flat_bigram_list)
+        most_common = bigram_counter.most_common(top_ngrams)
+
+    elif ngram_type == 'trigram':
+        trigram_list = [generate_trigrams(tokens) for tokens in token_list]
+        flat_trigram_list = [trigram for sublist in trigram_list for trigram in sublist]
+        trigram_counter = collections.Counter(flat_trigram_list)
+        most_common = trigram_counter.most_common(top_ngrams)
+    
+    else:
+        print('Please provide a n-gram type between 1 and 3')
+
+    word_counts = {}
+
+    for gram,count in most_common:
+        word_counts[' '.join(gram)] = count
+        
+    wordcloud = WordCloud(background_color="white").generate_from_frequencies(word_counts)
+    return wordcloud.to_image()
